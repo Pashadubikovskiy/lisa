fedb7d5a6032b2a29bee4716d7fdf73985803e54
Pasha <pashadubikovskiy@gmail.com>
2022-09-12 16:30:41 +0300

    materials page added

--- js/common.js
+++ js/common.js
@@ -1,12 +1,46 @@
 let loginForm = document.querySelector('.login__form');
-let preloader = document.querySelector('.preloader');
-console.log(preloader)
-if (preloader) {
-    preloader.classList.add('loaded');
-}
 if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        window.location.href = '/lisa/page.html'
+        window.location.href = '/lisa/materials.html'
+    })
+}
+
+let preloader = document.querySelector('.preloader');
+if (preloader) {
+    preloader.classList.add('loaded');
+}
+
+let burgerTriggers = document.querySelectorAll('.burger__btn, .burger-menu__close');
+let burgerMenu = document.querySelector('.burger__menu');
+if (burgerTriggers) {
+    burgerTriggers.forEach(el => {
+        el.addEventListener('click', (e) => {
+            burgerMenu.classList.toggle('burger__menu--is-opened');
+        })
+    })
+}
+
+let materialsCategories = document.querySelectorAll('.page-category__link');
+let materialImg = document.querySelectorAll('.material-table__img');
+let overlay = document.querySelector('.overlay');
+let overlayClose = document.querySelector('.overlay__close');
+if (materialsCategories) {
+    const materialsCategoryChange = (e) => {
+        e.preventDefault();
+        document.querySelector('.page-category__link--is-active').classList.remove('page-category__link--is-active')
+        e.target.classList.add('page-category__link--is-active')
+    }
+    materialsCategories.forEach((el)=>{
+        el.addEventListener('click', materialsCategoryChange)
+    })
+    materialImg.forEach(el=> {
+        el.addEventListener('click', ()=> {
+            overlay.classList.add('overlay--is-active');
+            overlay.querySelector('img').src = el.getAttribute('src')
+        })
+    })
+    overlayClose.addEventListener('click', ()=> {
+        overlay.classList.remove('overlay--is-active');
     })
 }
