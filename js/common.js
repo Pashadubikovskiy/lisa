b637a0f7f47ffa20e91f3a6d15ae72383d68865f
Pasha <pashadubikovskiy@gmail.com>
2023-04-28 18:29:10 +0300

    added pages

--- js/common.js
+++ js/common.js
@@ -21,26 +21,100 @@ if (burgerTriggers) {
     })
 }
 
-let materialsCategories = document.querySelectorAll('.page-category__link');
-let materialImg = document.querySelectorAll('.material-table__img');
-let overlay = document.querySelector('.overlay');
-let overlayClose = document.querySelector('.overlay__close');
-if (materialsCategories) {
-    const materialsCategoryChange = (e) => {
-        e.preventDefault();
-        document.querySelector('.page-category__link--is-active').classList.remove('page-category__link--is-active')
-        e.target.classList.add('page-category__link--is-active')
-    }
-    materialsCategories.forEach((el)=>{
-        el.addEventListener('click', materialsCategoryChange)
-    })
-    materialImg.forEach(el=> {
-        el.addEventListener('click', ()=> {
-            overlay.classList.add('overlay--is-active');
-            overlay.querySelector('img').src = el.getAttribute('src')
+if (document.querySelector('.overlay')) {
+    let materialsCategories = document.querySelectorAll('.page-category__link');
+    let materialImg = document.querySelectorAll('.materials-table__img, .recepts-table__img');
+    let overlay = document.querySelector('.overlay');
+    let overlayClose = document.querySelector('.overlay__close');
+    if (materialsCategories) {
+        const materialsCategoryChange = (e) => {
+            e.preventDefault();
+            document.querySelector('.page-category__link--is-active').classList.remove('page-category__link--is-active')
+            e.target.classList.add('page-category__link--is-active')
+        }
+        materialsCategories.forEach((el)=>{
+            el.addEventListener('click', materialsCategoryChange)
         })
-    })
-    overlayClose.addEventListener('click', ()=> {
-        overlay.classList.remove('overlay--is-active');
-    })
+        materialImg.forEach(el=> {
+            el.addEventListener('click', ()=> {
+                overlay.classList.add('overlay--is-active');
+                overlay.querySelector('img').src = el.getAttribute('src')
+            })
+        })
+        overlayClose.addEventListener('click', ()=> {
+            overlay.classList.remove('overlay--is-active');
+        })
+    }
+}
+
+// add new material
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", (e) => {
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", (e) => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach((type) => {
+        dropZoneElement.addEventListener(type, (e) => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        });
+    });
+
+    dropZoneElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove("drop-zone--over");
+    });
+});
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+        thumbnailElement = document.createElement("div");
+        thumbnailElement.classList.add("drop-zone__thumb");
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+        };
+    } else {
+        thumbnailElement.style.backgroundImage = null;
+    }
 }
